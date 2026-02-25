aed562970b984f4c466946c890ee19eea9b3a414
ansontansiths <ansont5@nycstudents.net>
2026-02-25 09:53:02 -0500

    Update numberguesser.py

--- numberguesser.py
+++ numberguesser.py
@@ -7,12 +7,12 @@ def numberguesser():
     guessed_correctly = False
     
     while not guessed_correctly:
-            user_guess = int(input("Enter your guess: "))
+            user_guess = int(input("Enter your guess:"))
             
             if user_guess < number_to_guess:
                 print("incorrect")
             elif user_guess > number_to_guess:
-                print("over but still incorrect")
+                print("incorrect")
             else:
                 guessed_correctly = True
                 print(f"You've guessed the number {number_to_guess}")
